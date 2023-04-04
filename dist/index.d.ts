3945cc7a467194fb2405fb4b6b924545ac66f1f1
Andre-John Mas <andrejohn.mas@gmail.com>
2023-04-04 11:50:51 -0400

    ts doesn't like package exports, so adjust exports

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import { TimerBasedCronScheduler } from './schedulers/timer-based.js';
+import { IntervalBasedCronScheduler } from './schedulers/interval-based.js';
+export * from './cron-parser.js';
+export * from './cron.js';
+export { TimerBasedCronScheduler, IntervalBasedCronScheduler };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+import { TimerBasedCronScheduler } from './schedulers/timer-based.js'
+import { IntervalBasedCronScheduler } from './schedulers/interval-based.js'
+
 export * from './cron-parser.js'
 export * from './cron.js'
+
+export { TimerBasedCronScheduler, IntervalBasedCronScheduler }
